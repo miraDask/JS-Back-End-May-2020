716c899862599e3758759fe1e0fb1dfe7ba10f4c
miraDask <mira.daskalov@gmail.com>
2020-05-20 23:50:55 +0100

    add breeds template

--- 01.Intro to Node.js - Cat Shelter Workshop/Cat Shelter/handlers/cats.js
+++ 01.Intro to Node.js - Cat Shelter Workshop/Cat Shelter/handlers/cats.js
@@ -6,32 +6,43 @@ const formidable = require('formidable');
 const cats = require('../data/cats.json');
 const breeds = require('../data/breeds.json');
 
-const createStreamForGet = (filePath, res) => {
-	const stream = fs.createReadStream(filePath);
-
-	stream.on('data', (data) => {
-		res.write(data);
-	});
-
-	stream.on('end', () => {
-		res.end();
-	});
-
-	stream.on('error', (err) => {
-		console.log(err);
-	});
-};
-
 module.exports = (req, res) => {
 	const pathName = url.parse(req.url).pathname;
 	const catsFilePath = path.normalize(path.join(__dirname, '../views/addCat.html'));
 	const breedsFilePath = path.normalize(path.join(__dirname, '../views/addBreed.html'));
+	const breedsDataFilePath = path.normalize(path.join(__dirname, '../data/breeds.json'));
 
 	if (pathName === '/cats/add-cat' && req.method === 'GET') {
-		createStreamForGet(catsFilePath, res);
+		const stream = fs.createReadStream(catsFilePath);
+
+		stream.on('data', (data) => {
+			const breedsPlaceholder = breeds.map((x) => `<option value="${x}">${x}</option>`);
+			const modifiedData = data.toString().replace('{{Breeds}}', breedsPlaceholder);
+			res.write(modifiedData);
+		});
+
+		stream.on('end', () => {
+			res.end();
+		});
+
+		stream.on('error', (err) => {
+			console.log(err);
+		});
 	} else if (pathName === '/cats/add-cat' && req.method === 'POST') {
 	} else if (pathName === '/cats/add-breed' && req.method === 'GET') {
-		createStreamForGet(breedsFilePath, res);
+		const stream = fs.createReadStream(breedsFilePath);
+
+		stream.on('data', (data) => {
+			res.write(data);
+		});
+
+		stream.on('end', () => {
+			res.end();
+		});
+
+		stream.on('error', (err) => {
+			console.log(err);
+		});
 	} else if (pathName === '/cats/add-breed' && req.method === 'POST') {
 		let formData = '';
 		req.on('data', (data) => {
@@ -40,7 +51,6 @@ module.exports = (req, res) => {
 
 		req.on('end', () => {
 			const body = querystring.parse(formData);
-			const breedsDataFilePath = path.normalize(path.join(__dirname, '../data/breeds.json'));
 			fs.readFile(breedsDataFilePath, (err, data) => {
 				if (err) {
 					console.log(err);
@@ -51,7 +61,7 @@ module.exports = (req, res) => {
 				breeds.push(body.breed);
 				const json = JSON.stringify(breeds);
 
-				fs.writeFile(breedsDataFilePath, json, () => console.log('breeds updated'));
+				fs.writeFile(breedsDataFilePath, json, 'utf-8', () => console.log('breeds updated'));
 			});
 
 			res.writeHead(302, {
